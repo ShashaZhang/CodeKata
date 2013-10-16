3a056fe6f456800cbd4d62a387ef34d8e2cc6422
HanKai <isaachanstar@gmail.com>
2013-10-16 20:15:36 +0800

    should return one temprature when there is one line validated.

--- CodeKata/4/WeatherDataFacts.cs
+++ CodeKata/4/WeatherDataFacts.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using Xunit;
 
 namespace _4
@@ -13,17 +14,31 @@ namespace _4
             IList<Temprature> tempratures = new DataReader().Read(new MemoryStream());
             Assert.Equal(0, tempratures.Count);
         }
+
+        [Fact]
+        public void should_return_tempratures_with_one_element_when_data_is_validated()
+        {
+            var tempratures = new DataReader().Read(new MemoryStream(Encoding.UTF8.GetBytes("1 23 8")));
+            Assert.Equal(1, tempratures.Count);
+        }
     }
 
     public class DataReader
     {
         public IList<Temprature> Read(Stream stream)
         {
+            var line = new StreamReader(stream).ReadLine();
+            if(line != null)
+                return new List<Temprature>(){ new Temprature(line)};
             return new List<Temprature>();
         }
     }
 
     public class Temprature
     {
+        public Temprature(string line)
+        {
+            
+        }
     }
 }
