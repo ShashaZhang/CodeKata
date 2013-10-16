533d23fbec51447d96c310f6e189433b51599264
HanKai <isaachanstar@gmail.com>
2013-10-16 20:10:52 +0800

    return empty list when there's no data in file.

--- CodeKata/4/WeatherDataFacts.cs
+++ CodeKata/4/WeatherDataFacts.cs
@@ -1,7 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Xunit;
+
 namespace _4
 {
     public class WeatherDataFacts
     {
-        
+        [Fact]
+        public void should_return_empty_list_when_data_is_empty()
+        {
+            IList<Temprature> tempratures = new DataReader().Read(new MemoryStream());
+            Assert.Equal(0, tempratures.Count);
+        }
+    }
+
+    public class DataReader
+    {
+        public IList<Temprature> Read(Stream stream)
+        {
+            return new List<Temprature>();
+        }
+    }
+
+    public class Temprature
+    {
     }
 }
