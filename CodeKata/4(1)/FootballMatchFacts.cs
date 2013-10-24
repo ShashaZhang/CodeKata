3b299d7f7e57ca17350a3b0d984fd66f6767889d
shasha <eileenss8824@gmail.com>
2013-10-23 18:54:25 -0700

    add test to make sure when there is both valid and invalid data, reader can get the right football match.

--- CodeKata/4(1)/FootballMatchFacts.cs
+++ CodeKata/4(1)/FootballMatchFacts.cs
@@ -53,6 +53,31 @@ namespace _4_1_
             Assert.Equal(0, matches.Count);
         }
 
+        [Fact]
+        public void should_be_able_to_parse_footballmatch_when_there_is_valid_data_and_invalid_data()
+        {
+            var matches = SetupMatches(@"
+                   Team            P     W    L   D    F      A     Pts
+    1. Arsenal         38    26   9   3    79  -  36    87
+    2. Liverpool       38    24   8   6    67  -  30    80
+    3. Manchester_U    38    24   5   9    87  -  45    77
+            ");
+
+            Assert.Equal(3, matches.Count);
+
+            Assert.Equal("Arsenal", matches[0].Name);
+            Assert.Equal(79, matches[0].Win);
+            Assert.Equal(36, matches[0].Lose);
+
+            Assert.Equal("Liverpool", matches[1].Name);
+            Assert.Equal(67, matches[1].Win);
+            Assert.Equal(30, matches[1].Lose);
+
+            Assert.Equal("Manchester_U", matches[2].Name);
+            Assert.Equal(87, matches[2].Win);
+            Assert.Equal(45, matches[2].Lose);
+        }
+
         private static IList<FootballMatch> SetupMatches(string dataStr)
         {
             return new DataReader(
